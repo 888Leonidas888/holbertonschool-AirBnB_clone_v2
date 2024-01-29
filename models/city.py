644eaf80389106c0e56e2131cde884b2146cfe32
Jhony <Jhonny14_1@hotmail.com>
2024-01-29 00:29:52 -0500

    fix: se agrega nuevo atributo

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-# from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 from os import getenv
 
 
@@ -12,6 +12,8 @@ if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship(
+            'Place', cascade='all, delete-orphan', backref='cities')
 else:
     class City(BaseModel):
         """ The city class, contains state ID and name """
