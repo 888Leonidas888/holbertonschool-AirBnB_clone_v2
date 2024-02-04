03a12623da7b8104be520da17259270b127ca969
Jhony <Jhonny14_1@hotmail.com>
2024-02-03 20:31:26 -0500

    chore: se agregan propiedades de campo

--- models/place.py
+++ models/place.py
@@ -10,9 +10,9 @@ from models.amenity import Amenity
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     place_amenity = Table('place_amenity', Base.metadata,
                           Column('place_id', String(60), ForeignKey(
-                              'places.id'), nullable=False),
+                              'places.id'), primary_key=True, nullable=False),
                           Column('amenity_id', String(60), ForeignKey(
-                              'amenities.id'), nullable=False))
+                              'amenities.id'), primary_key=True, nullable=False))
 
     class Place(BaseModel, Base):
         """ A place to stay """
